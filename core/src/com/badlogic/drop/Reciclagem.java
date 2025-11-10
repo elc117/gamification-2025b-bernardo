d09ecc099ce359379b99de2207a83b02d71e042e
Bernardo Trentin Bottega <bernardo.bottega@acad.ufsm.br>
2025-11-10 18:33:24 +0000

    Adicionado classe nova

---
+++ core/src/com/badlogic/drop/Reciclagem.java
@@ -0,0 +1,16 @@
+package com.badlogic.drop;
+
+import java.util.Iterator;
+
+class Reciclagem {
+    Texture imagem;
+    Rectangle objeto;
+    String tipo;
+
+    public Reciclagem(Texture imagem, Rectangle objeto, String tipo) {
+        this.imagem = imagem;
+        this.objeto = objeto;
+        this.tipo = tipo;
+    }
+
+}
