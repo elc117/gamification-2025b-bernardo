7e511b151c81a9c61b8d37dc03005c32d9004329
Bernardo Trentin Bottega <bernardo.bottega@acad.ufsm.br>
2025-11-14 18:00:23 +0000

    Atualizado imagens

--- core/src/com/badlogic/drop/ListaReciclagem.java
+++ core/src/com/badlogic/drop/ListaReciclagem.java
@@ -19,25 +19,25 @@ public class ListaReciclagem {
 
     // Métodos
     public void adicionarElementos() {
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("caixa-papelao.png")), new Rectangle(), "papel", 368, 20));
-        // lista.add(new Reciclagem(new Texture (Gdx.files.internal("papel.png")), new Rectangle(), "papel", 368, 20));
-        // lista.add(new Reciclagem(new Texture (Gdx.files.internal("revista.png")), new Rectangle(), "papel", 368, 20));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("caixa_papelao.png")), new Rectangle(), "papel", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("papel.png")), new Rectangle(), "papel", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("revista.png")), new Rectangle(), "papel", 500, 40));
 
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("copo-quebrado.png")), new Rectangle(), "vidro", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("garrafa-vidro.png")), new Rectangle(), "vidro", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("pote-vidro.png")), new Rectangle(), "vidro", 368, 20));
-
-
-        // lista.add(new Reciclagem(new Texture (Gdx.files.internal("maça.png")), new Rectangle(), "organico", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("ovo.png")), new Rectangle(), "organico", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("folha.png")), new Rectangle(), "organico", 368, 20));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("copo_quebrado.png")), new Rectangle(), "vidro", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("garrafa_vidro.png")), new Rectangle(), "vidro", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("pote_vidro.png")), new Rectangle(), "vidro", 500, 40));
 
 
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("tampa-garrafa.png")), new Rectangle(), "plastico", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("garrafa-pet.png")), new Rectangle(), "plastico", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("copo-descartavel.png")), new Rectangle(), "plastico", 368, 20));
-        // lista.add(new Reciclagem(new Texture (Gdx.files.internal("embalagem.png")), new Rectangle(), "plastico", 368, 20));
-        lista.add(new Reciclagem(new Texture (Gdx.files.internal("sacola-plastico.png")), new Rectangle(), "plastico", 368, 20));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("maca.png")), new Rectangle(), "organico", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("ovo.png")), new Rectangle(), "organico", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("folha.png")), new Rectangle(), "organico", 500, 40));
+
+
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("tampa_garrafa.png")), new Rectangle(), "plastico", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("garrafa_pet.png")), new Rectangle(), "plastico", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("copo_cafe.png")), new Rectangle(), "plastico", 500, 40));
+        // lista.add(new Reciclagem(new Texture (Gdx.files.internal("embalagem.png")), new Rectangle(), "plastico", 500, 40));
+        lista.add(new Reciclagem(new Texture (Gdx.files.internal("sacola_plastico.png")), new Rectangle(), "plastico", 500, 40));
 
 
     }
